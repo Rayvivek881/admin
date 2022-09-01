e972c4bfc8ef6a0ebfe00bddda5c3917b0282962
rayvivek881 <rayvivek881@gmail.com>
2022-09-01 09:19:35 +0530

    almost done

--- src/db/MySqlQuery.js
+++ src/db/MySqlQuery.js
@@ -1,14 +1,15 @@
 require('dotenv').config();
 const mysql = require('mysql')
+const { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } = process.env;
 var database;
 
-const connection = async () => {
+const connection = () => {
     database = mysql.createConnection({
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database : process.env.DB_DATABASE
+        host: DB_HOST,
+        port: DB_PORT,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database : DB_DATABASE
     });
 
     return new Promise((resolve, reject) => {
@@ -20,15 +21,14 @@ const connection = async () => {
     });
 };
 
-const query = async (sql) => {
-    const result = await new Promise((resolve, reject) => {
+const query = (sql) => {
+    return new Promise((resolve, reject) => {
         database.query(sql, (error, results) => {
             if (error != null) 
                 reject(error);
             else resolve(results);
         })
     });
-    return result;
 };
 const destroy = () => database.destroy();
 
